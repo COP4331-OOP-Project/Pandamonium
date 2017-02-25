29caa9715e0e2593c6b8794844786df96279a26f
Randy Brooks <Randy Brooks>
2017-02-25 16:33:50 -0500

    Commented Map Loader

--- src/main/game/gameboard/MapLoader.java
+++ src/main/game/gameboard/MapLoader.java
@@ -13,22 +13,22 @@ public final class MapLoader {
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			String currentLine;
 			int i = 0;
-			int invisibleLeft = boardSize/2;
+			int invisibleLeft = boardSize/2; //This is the number of invisible tiles on the left of the line
 			while ((currentLine = bufferedReader.readLine()) != null) { //Read a whole line from the file
 				String[] line = currentLine.split("\\s+"); //Split the line up by whitespace
 				int j = 0;
 				if (i % 2 == 0 && i != 0) { //The invisible tiles are staggered by 2
 					invisibleLeft--;
 				}
-				for (int left = 0; left < invisibleLeft; left++) {
+				for (int left = 0; left < invisibleLeft; left++) { //Adds invisible tiles on the left of the line
 					map[i][j] = -1;
 					j++;
 				}
-				for (int k = 0; k < line.length; k++) {
+				for (int k = 0; k < line.length; k++) {	//Loads actual map parts
 					map[i][j] = Integer.parseInt(line[k]);
 					j++;
 				}
-				while (j < boardSize) {
+				while (j < boardSize) { //Adds invisible tiles to the right of the line (line should only be as long as boardSize)
 					map[i][j] = -1;
 					j++;
 				}
