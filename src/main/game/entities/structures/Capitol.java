fd1c072992fab387359e197760eb8e0390c626ff
David Weng <dkweng@gmail.com>
2017-02-24 22:45:04 -0500

    Location Added, Updated Capitol and Structure

--- src/main/game/entities/structures/Capitol.java
+++ src/main/game/entities/structures/Capitol.java
@@ -1,7 +1,48 @@
 package game.entities.structures;
 
+import game.gameboard.Location;
+
+import java.util.ArrayList;
+
 /**
  * Created by David on 2/21/2017.
  */
 public class Capitol extends Structure {
+    //private ArrayList<worker> workers;
+    //private ArrayList<worker> farmer;
+    //private ArrayList<worker> generator;
+    //private ArrayList<worker> miner;
+    //private ArrayList<worker> breeder;
+
+    public Capitol(Location loc /*, EntityID entityID */){
+        super(loc);
+    }
+
+    public void assignToFarmer(){
+
+    }
+
+    public void assignToGenerator(){
+
+    }
+
+    public void assignToMiner(){
+
+    }
+
+    public void addWorker(){
+
+    }
+
+    public void removeWorker(){
+
+    }
+
+    public void breed(){
+
+    }
+
+    /*public Resource harvest(){
+
+    }*/
 }
--- src/main/game/entities/structures/Structure.java
+++ src/main/game/entities/structures/Structure.java
@@ -1,7 +1,42 @@
 package game.entities.structures;
 
+import game.commands.Command;
+import game.gameboard.Location;
+
 /**
  * Created by Alex on 2/21/17.
  */
 public class Structure {
+    //private EntityID entityID;
+    //private PowerState powerState;
+    //private float defenseDamage;
+    private Location location;
+
+    public Structure(){
+
+    }
+
+    public Structure(Location loc /*, EntityID entityID */){
+        location=loc;
+    }
+    //TODO add command queue? ASK TEAM IS THIS INTERFACE OR NOT?
+    public void addCommandToQueue(){
+
+    }
+
+    public void cancelQueueCommand(){
+
+    }
+    //TODO add power state
+    public void powerUp(){
+
+    }
+
+    public void standby(){
+
+    }
+
+    public void decomission(){
+
+    }
 }
