343d3ad579d8fbde2f0971d5fbb3f2635345c4c2
Khari Ollivierre <ollivierre.khari@gmail.com>
2017-02-25 12:52:18 -0500

    Added UnitStats

---
+++ src/main/game/entities/units/UnitStats.java
@@ -0,0 +1,29 @@
+package game.entities.units;
+
+/**
+ * Created by khariollivierre on 2/25/17.
+ */
+public class UnitStats {
+    private int offPow;
+    private int defPow;
+    private int armor;
+    private int speed;
+    private int health;
+    private float upkeep;
+
+    /* Accessors */
+    public int getOffPow() { return offPow; }
+    public int getDefPow() { return defPow; }
+    public int getArmor() { return armor; }
+    public int getSpeed() { return speed; }
+    public int getHealth() { return health; }
+    public float getUpkeep() { return upkeep; }
+
+    /* Mutators */
+    public void setOffPow(int offPow) { this.offPow = offPow; }
+    public void setDefPow(int defPow) { this.defPow = defPow; }
+    public void setArmor(int armor) { this.armor = armor; }
+    public void setSpeed(int speed) { this.speed = speed; }
+    public void setHealth(int health) { this.health = health; }
+    public void setUpkeep(float upkeep) { this.upkeep = upkeep; }
+}
