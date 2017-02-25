6d2b1d61c367fa9ec97e63c9a0bdca7d89398b23
David Weng <dkweng@gmail.com>
2017-02-25 16:25:37 -0500

    More functions added

--- src/main/game/entities/units/BattleGroupUnit.java
+++ src/main/game/entities/units/BattleGroupUnit.java
@@ -1,19 +1,30 @@
 package game.entities.units;
 
+import game.entities.BattleGroup;
+import game.entities.EntityId;
 import game.entities.iAttacker;
+import game.gameboard.Location;
 
 public class BattleGroupUnit implements iAttacker {
+    private EntityId entityId;
+    private UnitStats stats;
 
-    
+    public BattleGroupUnit(UnitStats stats, EntityId entityId){
+        this.stats = stats;
+        this.entityId = entityId;
+    }
 
     public double getDamage(){
-
-        return 0;
+        return stats.getOffPow();
     }
 
     public int getRange(){
-
+        //TODO Add Range
         return 0;
+    }
+
+    public int getMoveSpeed(){
+        return stats.getSpeed();
     }
 
 }
