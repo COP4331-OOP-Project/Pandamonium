c299c3061eea564da75e0e293de2ebf5652eedb2
Randy Brooks <randygeraldbrooks2@gmail.com>
2017-02-28 15:29:06 -0500

    Added To GameModel

--- src/main/game/GameModel.java
+++ src/main/game/GameModel.java
@@ -1,5 +1,77 @@
 package game;
 
-public class GameModel {
+import java.util.ArrayList;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import controls.command.CommandController;
+import game.commands.MoveCommand;
+import game.entities.EntityId;
+import game.gameboard.Gameboard;
+import game.gameboard.Location;
+
+public class GameModel {
+	private static final Location HUMAN_STARTING_LOCATION = new Location(5, 28);
+	private static final Location PANDA_STARTING_LOCATION = new Location(32, 11);
+    private final static Logger log = LogManager.getLogger(CommandController.class);
+    private Player currentPlayer;
+    private ArrayList<Player> players;
+    private Gameboard gBoard;
+    private Player nextPlayer;
+    private int turnNum;
+    private ArrayList<MoveCommand> moveCommands = new ArrayList<MoveCommand>();
+    private ArrayList<Location> moveLocations = new ArrayList<Location>();
+    private Location lastMoveLocation;
+    private EntityId selectedEntity; 
+    private boolean gameHasStarted = false;
+    
+    public void initializeGame() {
+        this.players = new ArrayList<Player>();
+        Player human = new Player(0, HUMAN_STARTING_LOCATION);
+        Player panda = new Player(1, PANDA_STARTING_LOCATION);
+        players.add(human);
+        players.add(panda);
+        gBoard = new Gameboard(players);
+        currentPlayer = human;
+        nextPlayer = players.get(1);
+        turnNum = 1;
+        gameHasStarted =  true;
+    }
+    
+    public void updateGame() { //This is called up to 60 times per second
+    	if (gameHasStarted) {
+    		checkIfGameOver();
+    	}
+    }
+    
+    private void checkIfGameOver() {
+	}
+    
+    public void endTurn() {
+    }
+
+	public ArrayList<Location> getMoveLocations() {
+        return moveLocations;
+    }
+	
+    public int getTurnNum() {
+        return turnNum;
+    }
+    
+    public Gameboard getGameboard() {
+        return gBoard;
+    }
+    
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+    
+    public Player getPlayer(int playerID) {
+        return players.get(playerID);
+    }
+    
+    public EntityId getSelectedEntity() {
+    	return selectedEntity;
+    }
 }
--- src/main/game/gameboard/Gameboard.java
+++ src/main/game/gameboard/Gameboard.java
@@ -1,7 +1,14 @@
 package game.gameboard;
 
+import java.util.ArrayList;
+
+import game.Player;
+
 /**
  * Created by Alex on 2/21/17.
  */
 public class Gameboard {
+
+	public Gameboard(ArrayList<Player> players) {
+	}
 }
