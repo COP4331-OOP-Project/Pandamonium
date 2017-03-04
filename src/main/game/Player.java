710741903b240386dd3a7a13ef435fcfe134d65f
Randy Brooks <randygeraldbrooks2@gmail.com>
2017-03-03 23:40:42 -0500

    Player Class Changes

--- src/main/game/Player.java
+++ src/main/game/Player.java
@@ -13,7 +13,14 @@ import game.entities.structures.Fort;
 import game.entities.structures.Mine;
 import game.entities.structures.ObservationTower;
 import game.entities.structures.PowerPlant;
-import game.entities.factories.EntityFactory;
+import game.entities.Army;
+import game.entities.EntitySubtypeEnum;
+import game.entities.RallyPoint;
+import game.entities.factories.UnitFactory;
+import game.entities.factories.exceptions.ColonistLimitExceededException;
+import game.entities.factories.exceptions.ExplorerLimitExceededException;
+import game.entities.factories.exceptions.MeleeLimitExceededException;
+import game.entities.factories.exceptions.RangedLimitExceededException;
 import game.entities.structures.Structure;
 import game.entities.structures.University;
 import game.entities.units.Colonist;
@@ -21,7 +28,10 @@ import game.entities.units.Explorer;
 import game.entities.units.Melee;
 import game.entities.units.Ranged;
 import game.entities.units.Unit;
+import game.entities.units.exceptions.UnitNotFoundException;
+import game.entities.workers.workerTypes.Worker;
 import game.gameboard.Location;
+import game.gameboard.SimpleTile;
 
 
 public class Player {
@@ -31,45 +41,44 @@ public class Player {
     private int playerId;
 
     //ArrayLists of this player's instances
-    //private ArrayList<Army> armies;
+    private ArrayList<Army> armies;
     private ArrayList<Melee> melees;
     private ArrayList<Ranged> ranges;
     private ArrayList<Explorer> explorers;
     private ArrayList<Colonist> colonists;
-    //private ArrayList<Worker> workers;
+    private ArrayList<Worker> workers;
     private ArrayList<Structure> structures;
-    //private ArrayList<RallyPoint> rallyPoints;
+    private ArrayList<RallyPoint> rallyPoints;
     private ArrayList<Unit> totalUnits;
     private int totalUnitCount;
-    //private SimpleMap
-
-
+    private SimpleTile[][] simpleTiles;
+    private UnitFactory unitFactory = new UnitFactory();
     //Initial Units
     private Colonist initialColonist;
 
     public Player(int playerId, Location loc) {
+    	
         //loc is referring to the starting location of this player
         this.playerId = playerId;
-
-        try {
-            //initialColonist = (Colonist)EntityFactory.getEntity(loc, this.playerId, "c")
-        } catch (UnknownEntityException e){
-            log.error(e.getLocalizedMessage());
-        } finally {
-            init();
-        }
+		try {
+			initialColonist = (Colonist)unitFactory.createUnit(EntitySubtypeEnum.COLONIST, loc, playerId);
+		} catch (ColonistLimitExceededException | ExplorerLimitExceededException | RangedLimitExceededException
+				| MeleeLimitExceededException | UnitNotFoundException e) {
+			e.printStackTrace();
+		}
+        init();
     }
 
     private void init(){
         //Initialize all the arraylists of thsi player
-        //armies = new ArrayList<Army>();
+        armies = new ArrayList<Army>();
         melees = new ArrayList<Melee>();
         ranges = new ArrayList<Ranged>();
         explorers = new ArrayList<Explorer>();
         colonists = new ArrayList<Colonist>();
-        //workers = new ArrayList<Worker>();
+        workers = new ArrayList<Worker>();
         structures = new ArrayList<Structure>();
-        //rallyPoints = new ArrayList<RallyPoint>();
+        rallyPoints = new ArrayList<RallyPoint>();
         totalUnits = new ArrayList<Unit>();
     }
 
@@ -89,65 +98,42 @@ public class Player {
         colonists.add(colonist);
     }
 
-    /*
     public void addWorker(Worker worker){
         workers.add(worker);
     }
-    */
 
     public void removeMelee(Melee melee){
-//        for(int i = 0;i<melees.size();i++){
-//            if(melees.get(i).getInstanceId() == melee.getInstanceId()){
-//                melees.remove(i);
-//            }
-//        }
+    	melees.remove(melee);
     }
 
     public void removeRanged(Ranged ranged){
-//        for(int i = 0;i<ranges.size();i++){
-//            if(ranges.get(i).getInstanceId() == ranges.getInstanceId()){
-//                ranges.remove(i);
-//            }
-//        }
+    	ranges.remove(ranged);
     }
 
     public void removeExplorer(Explorer explorer){
-//        for(int i = 0;i<explorers.size();i++){
-//            if(explorers.get(i).getInstanceId() == explorer.getInstanceId()){
-//                explorers.remove(i);
-//            }
-//        }
+    	explorers.remove(explorer);
     }
 
     public void removeColonist(Colonist colonist){
-//        for(int i = 0;i<colonists.size();i++){
-//            if(colonists.get(i).getInstanceId() == colonist.getInstanceId()){
-//                colonists.remove(i);
-//            }
-//        }
+    	colonists.remove(colonist);
     }
-/*
-    public void remvoeWorker(Worker worker){
-        for(int i = 0;i<melees.size();i++){
-            if(workers.get(i).getInstanceId() == worker.getInstanceId()){
-                workers.remove(i);
-            }
-        }
+    
+    public void removeWorker(Worker worker){
+        workers.remove(worker);
     }
-*/
-    /*
+    
+    
     public void addArmy(Army army){
         armies.add(army);
     }
 
     public void addRallyPoint(RallyPoint rallyPoint){
-        rallyPoint.add(rallyPoint);
+        rallyPoints.add(rallyPoint);
     }
-    */
-    /*
+   
     public void removeArmy(Army army){
         armies.set(army.getInstanceId(), null);
-    }*/
+    }
 
     public ArrayList<Melee> getMelees() {
         return melees;
@@ -168,10 +154,10 @@ public class Player {
     public ArrayList<Unit> getAllUnit() {
         return totalUnits;
     }
-    /*
+    
     public ArrayList<Worker> getWorkers(){
         return workers;
-    }*/
+    }
 
     public void addCapitol(Capitol capitol){
         structures.add(capitol);
@@ -202,58 +188,30 @@ public class Player {
     }
 
     public void removeCapitol(Capitol capitol){
-        for(int i = 0;i<structures.size();i++){
-            if(structures.get(i).getInstanceId() == capitol.getInstanceId()){
-                structures.remove(i);
-            }
-        }
+        structures.remove(capitol);
     }
-
+    
     public void removeFarm(Farm farm){
-        for(int i = 0;i<structures.size();i++){
-            if(structures.get(i).getInstanceId() == farm.getInstanceId()){
-                structures.remove(i);
-            }
-        }
+        structures.remove(farm);
     }
 
     public void removeFort(Fort fort){
-        for(int i = 0;i<structures.size();i++){
-            if(structures.get(i).getInstanceId() == fort.getInstanceId()){
-                structures.remove(i);
-            }
-        }
+        structures.remove(fort);
     }
 
     public void removeMine(Mine mine){
-        for(int i = 0;i<structures.size();i++){
-            if(structures.get(i).getInstanceId() == mine.getInstanceId()){
-                structures.remove(i);
-            }
-        }
+        structures.remove(mine);
     }
 
     public void removeObservationTower(ObservationTower tower){
-        for(int i = 0;i<structures.size();i++){
-            if(structures.get(i).getInstanceId() == tower.getInstanceId()){
-                structures.remove(i);
-            }
-        }
+        structures.remove(tower);
     }
 
     public void removePowerPlant(PowerPlant powerPlant){
-        for(int i = 0;i<structures.size();i++){
-            if(structures.get(i).getInstanceId() == powerPlant.getInstanceId()){
-                structures.remove(i);
-            }
-        }
+        structures.remove(powerPlant);
     }
 
     public void removeUniversity(University university){
-        for(int i = 0;i<structures.size();i++){
-            if(structures.get(i).getInstanceId() == university.getInstanceId()){
-                structures.remove(i);
-            }
-        }
+        structures.remove(university);
     }
 }
--- src/main/game/entities/factories/EntityFactory.java
+++
@@ -1,4 +0,0 @@
-package game.entities.factories;
-
-// Factory to create entities for the game
-public class EntityFactory {}
