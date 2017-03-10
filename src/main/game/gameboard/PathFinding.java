96abf46783262cfa2d28d0573fd1d38e5489841b
David Weng <dkweng@gmail.com>
2017-03-10 16:24:27 -0500

    Add heuristic helper helper functions

--- src/main/game/gameboard/PathFinding.java
+++ src/main/game/gameboard/PathFinding.java
@@ -17,6 +17,33 @@ public class PathFinding {
     }
 
     public int estimateCost(Location start, Location end){
+        Location current = start;
+
         return 0;
+    }
+
+    public int howClose(Location current, Location end){
+        int x = Math.abs(current.getX()-end.getX());
+        int y = Math.abs(current.getY()-end.getY());
+        return x+y;
+    }
+
+    public Location directionLocation(Location current, int direction){
+        switch (direction){
+            case 0:
+                return new Location(current.getX(), current.getY()-1);
+            case 45:
+                return new Location(current.getX()+1, current.getY()-1);
+            case 135:
+                return new Location(current.getX()+1, current.getY());
+            case 180:
+                return new Location(current.getX(), current.getY()+1);
+            case 225:
+                return new Location(current.getX()-1, current.getY()+1);
+            case 315:
+                return new Location(current.getX()-1, current.getY());
+        }
+
+        return null;
     }
 }
