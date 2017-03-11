abc07f27bff77c7dac16ab9272f7933792f2e2a5
Randy Brooks <randygeraldbrooks2@gmail.com>
2017-03-11 16:46:11 -0500

    Unit Overview

--- src/main/view/game/StructureOverviewPanel.java
+++ src/main/view/game/StructureOverviewPanel.java
@@ -2,32 +2,69 @@ package view.game;
 
 import java.awt.Point;
 
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.text.Font;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.paint.Color;
 import view.GameModelAdapter;
 import view.ViewEnum;
 import view.assets.AssetManager;
 
-public class StructureOverviewPanel extends OverviewPanel {
-	public StructureOverviewPanel(GameModelAdapter gameModelAdapter, AssetManager assets,
-			ViewEnum view) {
+public class StructureOverviewPanel extends OverviewPanel{
+	private static final int PANE_WIDTH = 219;
+	private static final int PANE_HEIGHT = 80;
+	private ScrollPane scrollPane = new ScrollPane();
+	private GraphicsContext overviewGraphics;
+	private Canvas canvas;
+	private Group root;
+	
+	public StructureOverviewPanel(GameModelAdapter gameModelAdapter, AssetManager assets, ViewEnum view, Group root) {
 		super(gameModelAdapter, assets, view);
-		setIsVisible(false);
+		this.root = root;
+		scrollPane.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+		scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
+		canvas = new Canvas(); //This is the canvas that goes inside of the scroll pane
+		overviewGraphics = canvas.getGraphicsContext2D();
+		scrollPane.setContent(canvas);
+		canvas.setOnMouseClicked(event -> paneClicked(event.getX(), event.getY()));
+		scrollPane.addEventFilter(ScrollEvent.SCROLL,event -> {
+		    if (event.getDeltaX() != 0) { 
+		        event.consume(); //This disables vertical scrolling in the scroll pane
+		    }
+		});
+		 //This sets the style of scrollPane to that specified in the CSS document
+		scrollPane.getStyleClass().setAll("scroll");
+		overviewGraphics.setFill(Color.WHITE);
 	}
 
 	public void draw(GraphicsContext g, Point screenDimensions, long currentPulse) {
-		drawPanelBox(g, screenDimensions);
-		Font oldFont = g.getFont();
-		g.setFont(getAssets().getFont(2));
-		g.fillText("Structure Overview", screenDimensions.x / 2 - 370,
-				screenDimensions.y / 2 - 245);
-		g.setFont(getAssets().getFont(1));
-		g.setFont(oldFont);
+		scrollPane.toFront();
+		overviewGraphics.clearRect(0, 0, 2500, screenDimensions.y);
+		scrollPane.setMaxWidth(screenDimensions.x - 148);
+		scrollPane.setMaxHeight(488);
+		scrollPane.setTranslateX(74);
+		scrollPane.setTranslateY(50);
+		canvas.setWidth(screenDimensions.x - 148);
+		canvas.setHeight(3000);
 	}
-
-	public void hideGUIElements() {
+	
+	private void paneClicked(double x, double y) {
+		//if (pointInPane(14, 87, x, y))
+	}
+	
+	private boolean pointInPane(int paneX, int paneY, double clickX, double clickY) {
+		return ((clickX >= paneX && clickX <= paneX + PANE_WIDTH) && 
+				(clickY >= paneY && clickY <= paneY + PANE_HEIGHT));
 	}
 
 	public void showGUIElements() {
+		root.getChildren().add(scrollPane);
+	}
+
+	public void hideGUIElements() {
+		root.getChildren().remove(scrollPane);
 	}
 }
--- src/main/view/game/UnitOverviewPanel.java
+++ src/main/view/game/UnitOverviewPanel.java
@@ -2,31 +2,109 @@ package view.game;
 
 import java.awt.Point;
 
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.text.Font;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import view.GameModelAdapter;
 import view.ViewEnum;
 import view.assets.AssetManager;
 
-public class UnitOverviewPanel extends OverviewPanel {
-
-	public UnitOverviewPanel(GameModelAdapter gameModelAdapter, AssetManager assets,
-			ViewEnum viewEnum) {
-		super(gameModelAdapter, assets, viewEnum);
-		setIsVisible(false);
+public class UnitOverviewPanel extends OverviewPanel{
+	private static final int PANE_WIDTH = 219;
+	private static final int PANE_HEIGHT = 80;
+	private final TableView unitTable = new TableView();
+	private final VBox unitBox = new VBox();
+	private DropShadow ds = new DropShadow();
+	private ScrollPane scrollPane = new ScrollPane();
+	private GraphicsContext overviewGraphics;
+	private Canvas canvas;
+	private Group root;
+	
+	public UnitOverviewPanel(GameModelAdapter gameModelAdapter, AssetManager assets, ViewEnum view, Group root) {
+		super(gameModelAdapter, assets, view);
+		this.root = root;
+		scrollPane.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+		scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
+		ds.setOffsetY(3.0);
+		ds.setRadius(3);
+		ds.setColor(Color.color(0, 0, 0));
+		final Label label = new Label("Unit Overview");
+		label.setTextFill(Color.WHITE);
+        label.setFont(getAssets().getFont(3));
+        label.setEffect(ds);
+		final TextField unitType = new TextField();
+		final TextField health = new TextField();
+		final TextField attack = new TextField();
+		final TextField defense = new TextField();
+		final TextField armor = new TextField();
+		final TextField upkeep = new TextField();
+		TableColumn unitTypeColumn = new TableColumn("Type");
+		TableColumn healthColumn = new TableColumn("Health");
+		TableColumn attackColumn = new TableColumn("Attack");
+		TableColumn defenseColumn = new TableColumn("Defense");
+		TableColumn armorColumn = new TableColumn("Armor");
+		TableColumn upkeepColumn = new TableColumn("Upkeep");
+		//unitTypeColumn.setResizable(false);
+		//healthColumn.setResizable(false);
+		//attackColumn.setResizable(false);
+		//defenseColumn.setResizable(false);
+		//armorColumn.setResizable(false);
+		//upkeepColumn.setResizable(false);
+		unitTable.setEditable(false);
+		unitTable.getColumns().addAll(unitTypeColumn, healthColumn, attackColumn, defenseColumn, armorColumn, upkeepColumn);
+		canvas = new Canvas(); //This is the canvas that goes inside of the scroll pane
+		overviewGraphics = canvas.getGraphicsContext2D();
+		scrollPane.setContent(unitBox);
+		canvas.setOnMouseClicked(event -> paneClicked(event.getX(), event.getY()));
+		scrollPane.addEventFilter(ScrollEvent.SCROLL,event -> {
+		    if (event.getDeltaX() != 0) { 
+		        event.consume(); //This disables vertical scrolling in the scroll pane
+		    }
+		});
+		unitBox.setSpacing(5);
+        unitBox.setPadding(new Insets(10, 0, 0, 10));
+        unitBox.getChildren().addAll(label, unitTable);
+		 //This sets the style of scrollPane to that specified in the CSS document
+		scrollPane.getStyleClass().setAll("scroll");
+		overviewGraphics.setFill(Color.WHITE);
 	}
 
 	public void draw(GraphicsContext g, Point screenDimensions, long currentPulse) {
-		drawPanelBox(g, screenDimensions);
-		Font oldFont = g.getFont();
-		g.setFont(getAssets().getFont(2));
-		g.fillText("Unit Overview", screenDimensions.x / 2 - 370, screenDimensions.y / 2 - 245);
-		g.setFont(getAssets().getFont(1));
+		scrollPane.toFront();
+		overviewGraphics.clearRect(0, 0, 2500, screenDimensions.y);
+		scrollPane.setMaxWidth(screenDimensions.x - 148);
+		scrollPane.setMaxHeight(488);
+		scrollPane.setTranslateX(74);
+		scrollPane.setTranslateY(50);
+		unitTable.setPrefWidth(screenDimensions.x - 148);
+		canvas.setHeight(3000);
 	}
-
-	public void hideGUIElements() {
+	
+	private void paneClicked(double x, double y) {
+		//if (pointInPane(14, 87, x, y))
+	}
+	
+	private boolean pointInPane(int paneX, int paneY, double clickX, double clickY) {
+		return ((clickX >= paneX && clickX <= paneX + PANE_WIDTH) && 
+				(clickY >= paneY && clickY <= paneY + PANE_HEIGHT));
 	}
 
 	public void showGUIElements() {
+		root.getChildren().add(scrollPane);
+	}
+
+	public void hideGUIElements() {
+		root.getChildren().remove(scrollPane);
 	}
 }
