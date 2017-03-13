674966a28bc965392613347eb07ec42914f9b944
R. Alex Clark <aclark2089@gmail.com>
2017-03-12 20:07:35 -0400

    Added visitors for each of the notifying research events for units and structs

---
+++ src/main/entityResearch/researchVisitors/UnitMovementRangeResearchVisitor.java
@@ -0,0 +1,29 @@
+package entityResearch.researchVisitors;
+
+import entityResearch.iUnitResearchVisitor;
+import game.entities.EntitySubtypeEnum;
+import game.entities.factories.exceptions.UnitTypeDoesNotExistException;
+import game.entities.managers.UnitManager;
+import game.semantics.Percentage;
+
+// Visitor to have the unit manager of player perform the upgrade on subtype movement range
+public class UnitMovementRangeResearchVisitor implements iUnitResearchVisitor {
+
+    private int increaseAmount;         // Movement range increase amount
+    private EntitySubtypeEnum subtype;  // Unit subtype
+
+    // Constructor
+    public UnitMovementRangeResearchVisitor(EntitySubtypeEnum subtype, int increaseAmount) {
+        this.increaseAmount = increaseAmount;
+        this.subtype = subtype;
+    }
+
+    // Visit unit manager to update subtype's movement range
+    public void visitUnitManager(UnitManager unitManager) throws UnitTypeDoesNotExistException {
+        unitManager.increaseMovementRange(this.subtype, this.increaseAmount);
+    }
+
+}
+
+
+
