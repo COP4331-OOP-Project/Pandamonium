b86e06e6752a87d2528aed3e8bdef358230b25c2
Gavin Scheele <gdscheele@gmail.com>
2017-03-13 18:55:08 -0400

    Gavin - research for each entity type

---
+++ src/main/game/entityTypeResearch/treeTypes/structureAdvancements/StructureHealthAdvancementTree.java
@@ -0,0 +1,26 @@
+package game.entityTypeResearch.treeTypes.structureAdvancements;
+
+import game.entities.EntitySubtypeEnum;
+import game.entities.managers.StructureManager;
+import game.entityTypeResearch.nodeTypes.EntityTypeAdvancementNode;
+import game.entityTypeResearch.nodeTypes.structureAdvancements.StructureDefensiveAdvancementNode;
+import game.entityTypeResearch.nodeTypes.structureAdvancements.StructureHealthAdvancementNode;
+
+public class StructureHealthAdvancementTree {
+
+    private EntityTypeAdvancementNode root;
+    public StructureHealthAdvancementTree(StructureManager structureManager, EntitySubtypeEnum unitType) {
+        StructureHealthAdvancementNode root = new StructureHealthAdvancementNode(structureManager, unitType, 1, "Health Increase #1", "+1");
+        StructureHealthAdvancementNode second = new StructureHealthAdvancementNode(structureManager, unitType, 1, "Health Increase #2", "+1", root);
+        StructureHealthAdvancementNode third = new StructureHealthAdvancementNode(structureManager, unitType, 1, "Health Increase #3", "+1", second);
+
+        root.setChild(second);
+        second.setChild(third);
+
+        this.root = root;
+    }
+
+    public EntityTypeAdvancementNode getRoot() {
+        return this.root;
+    }
+}
---
+++ src/main/game/entityTypeResearch/treeTypes/unitAdvancements/UnitEfficiencyAdvancementTree.java
@@ -0,0 +1,34 @@
+package game.entityTypeResearch.treeTypes.unitAdvancements;
+
+import game.entities.EntitySubtypeEnum;
+import game.entities.managers.UnitManager;
+import game.entityTypeResearch.nodeTypes.EntityTypeAdvancementNode;
+import game.entityTypeResearch.nodeTypes.unitAdvancements.UnitEfficiencyAdvancementNode;
+import game.semantics.Percentage;
+import game.semantics.PercentageOutOfRangeException;
+
+public class UnitEfficiencyAdvancementTree {
+
+    private EntityTypeAdvancementNode root;
+    public UnitEfficiencyAdvancementTree(UnitManager unitManager, EntitySubtypeEnum unitType) {
+        Percentage tenPercent;
+
+        try {
+            tenPercent = new Percentage(.1);
+        } catch (PercentageOutOfRangeException e) {
+            throw new RuntimeException("Percentage out of range");
+        }
+        UnitEfficiencyAdvancementNode root = new UnitEfficiencyAdvancementNode(unitManager, unitType, tenPercent, "Efficiency Increase #1", "+1");
+        UnitEfficiencyAdvancementNode second = new UnitEfficiencyAdvancementNode(unitManager, unitType, tenPercent, "Efficiency Increase #2", "+1", root);
+        UnitEfficiencyAdvancementNode third = new UnitEfficiencyAdvancementNode(unitManager, unitType, tenPercent, "Efficiency Increase #3", "+1", second);
+
+        root.setChild(second);
+        second.setChild(third);
+
+        this.root = root;
+    }
+
+    public EntityTypeAdvancementNode getRoot() {
+        return this.root;
+    }
+}
