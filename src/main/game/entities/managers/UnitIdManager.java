e2b279a9dbd56f4d5d10ac8b40a2bbca08ae647f
R. Alex Clark <aclark2089@gmail.com>
2017-03-13 11:20:02 -0400

    Fixed UnitIdManager's logger to use correct class name

--- src/main/game/entities/managers/UnitIdManager.java
+++ src/main/game/entities/managers/UnitIdManager.java
@@ -20,7 +20,7 @@ import org.apache.logging.log4j.Logger;
 
 public class UnitIdManager {
 
-    private final static Logger log = LogManager.getLogger(UnitFactory.class);
+    private final static Logger log = LogManager.getLogger(UnitIdManager.class);
 
     // Static limits on unit counts
     private static final Integer MAX_UNIT_COUNT = 25;
