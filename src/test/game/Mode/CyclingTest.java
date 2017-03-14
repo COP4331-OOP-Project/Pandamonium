2a354db55b0580cf1711f3d1798413b0d93a4fe2
karthshen <karth-shen@hotmail.com>
2017-03-14 18:23:04 -0400

    Test for cycle submode

--- src/test/game/Mode/CyclingTest.java
+++ src/test/game/Mode/CyclingTest.java
@@ -3,6 +3,7 @@ package game.Mode;
 import game.GameModel;
 import game.mode.Mode;
 import game.mode.ModeController;
+import game.mode.Submode;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -53,9 +54,62 @@ public class CyclingTest {
         Assert.assertEquals(Mode.UNIT, modeController.getGameMode());
     }
 
-    @Test //Test submode
+    @Test //Test submode forward
     public void testCycleSubmodeForward(){
         modeController.setMode(Mode.UNIT);
-        
+        Assert.assertEquals(Mode.UNIT, modeController.getGameMode());
+        Assert.assertEquals(Submode.EXPLORER, modeController.getGameSubmode());
+
+        modeController.cycleSubmodeForward();
+        Assert.assertEquals(Submode.COLONIST, modeController.getGameSubmode());
+        modeController.cycleSubmodeForward();
+        Assert.assertEquals(Submode.MELEE, modeController.getGameSubmode());
+        modeController.cycleSubmodeForward();
+        Assert.assertEquals(Submode.RANGED, modeController.getGameSubmode());
+        modeController.cycleSubmodeForward();
+        Assert.assertEquals(Submode.EXPLORER, modeController.getGameSubmode());
+
+        modeController.setMode(Mode.STRUCTURE);
+        Assert.assertEquals(Mode.STRUCTURE, modeController.getGameMode());
+        Assert.assertEquals(Submode.CAPITOL, modeController.getGameSubmode());
+
+        modeController.cycleSubmodeForward();
+        Assert.assertEquals(Submode.FARM, modeController.getGameSubmode());
+        modeController.cycleSubmodeForward();
+        Assert.assertEquals(Submode.FORT, modeController.getGameSubmode());
+        modeController.cycleSubmodeForward();
+        Assert.assertEquals(Submode.MINE, modeController.getGameSubmode());
+        modeController.cycleSubmodeForward();
+        Assert.assertEquals(Submode.OBSERVE, modeController.getGameSubmode());
+        modeController.cycleSubmodeForward();
+        Assert.assertEquals(Submode.PLANT, modeController.getGameSubmode());
+        modeController.cycleSubmodeForward();
+        Assert.assertEquals(Submode.UNIVERSITY, modeController.getGameSubmode());
+
+    }
+
+    @Test //Test submode backward
+    public void testCycleSubModeBackward(){
+        modeController.setMode(Mode.UNIT);
+        Assert.assertEquals(Mode.UNIT, modeController.getGameMode());
+        Assert.assertEquals(Submode.EXPLORER, modeController.getGameSubmode());
+
+        modeController.cycleSubmodeBackward();
+        Assert.assertEquals(Submode.RANGED, modeController.getGameSubmode());
+        modeController.cycleSubmodeBackward();
+        Assert.assertEquals(Submode.MELEE, modeController.getGameSubmode());
+        modeController.cycleSubmodeBackward();
+        Assert.assertEquals(Submode.COLONIST, modeController.getGameSubmode());
+        modeController.cycleSubmodeBackward();
+        Assert.assertEquals(Submode.EXPLORER, modeController.getGameSubmode());
+
+        modeController.setMode(Mode.ARMY);
+        Assert.assertEquals(Submode.ENTIRE_ARMY, modeController.getGameSubmode());
+        modeController.cycleSubmodeBackward();
+        Assert.assertEquals(Submode.REINFORCEMENTS, modeController.getGameSubmode());
+        modeController.cycleSubmodeBackward();
+        Assert.assertEquals(Submode.BATTLE_GROUP, modeController.getGameSubmode());
+        modeController.cycleSubmodeBackward();
+        Assert.assertEquals(Submode.ENTIRE_ARMY, modeController.getGameSubmode());
     }
 }
